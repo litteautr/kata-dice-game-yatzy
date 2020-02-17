985987c7f81c04a12d1cbac3e51db2082e931626
Robin Litteaut <litteautr@gmail.com>
2020-02-17 18:35:27 +0100

    Renaming / Refactoring tests

--- src/test/java/fr/rli/yatzy/YatzyTest.java
+++ src/test/java/fr/rli/yatzy/YatzyTest.java
@@ -3,118 +3,123 @@ package fr.rli.yatzy;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class YatzyTest {
 
     @Test
-    public void chance_scores_sum_of_all_dice() {
-        int expected = 15;
-        int actual = Yatzy.chance(2, 3, 4, 5, 1);
-        assertEquals(expected, actual);
-        assertEquals(16, Yatzy.chance(3, 3, 4, 5, 1));
+    public void shoud_sum_all_dice_for_a_chance_roll() {
+        assertEquals(15, Yatzy.chance(2, 3, 4, 5, 1));
     }
 
     @Test
-    public void yatzy_scores_50() {
-        int expected = 50;
-        int actual = Yatzy.yatzy(4, 4, 4, 4, 4);
-        assertEquals(expected, actual);
-        assertEquals(50, Yatzy.yatzy(6, 6, 6, 6, 6));
+    public void should_score_50_for_a_yatzy_roll() {
+        assertEquals(50, Yatzy.yatzy(4, 4, 4, 4, 4));
+    }
+
+    @Test
+    public void should_score_50_for_a_missed_yatzy_roll() {
         assertEquals(0, Yatzy.yatzy(6, 6, 6, 6, 3));
     }
 
+
     @Test
-    public void test_1s() {
-        assertTrue(Yatzy.ones(1, 2, 3, 4, 5) == 1);
+    public void should_sum_all_one_dice_for_a_ones_roll() {
         assertEquals(2, Yatzy.ones(1, 2, 1, 4, 5));
         assertEquals(0, Yatzy.ones(6, 2, 2, 4, 5));
-        assertEquals(4, Yatzy.ones(1, 2, 1, 1, 1));
     }
 
     @Test
-    public void test_2s() {
+    public void should_sum_all_two_dice_for_a_twos_roll() {
         assertEquals(4, Yatzy.twos(1, 2, 3, 2, 6));
-        assertEquals(10, Yatzy.twos(2, 2, 2, 2, 2));
     }
 
     @Test
-    public void test_threes() {
+    public void should_sum_all_three_dice_for_a_threes_roll() {
         assertEquals(6, Yatzy.threes(1, 2, 3, 2, 3));
-        assertEquals(12, Yatzy.threes(2, 3, 3, 3, 3));
     }
 
     @Test
-    public void fours_test() {
+    public void should_sum_all_four_dice_for_a_fours_roll() {
         assertEquals(12, new Yatzy(4, 4, 4, 5, 5).fours());
-        assertEquals(8, new Yatzy(4, 4, 5, 5, 5).fours());
-        assertEquals(4, new Yatzy(4, 5, 5, 5, 5).fours());
     }
 
     @Test
-    public void fives() {
+    public void should_sum_all_five_dice_for_a_five_roll() {
         assertEquals(10, new Yatzy(4, 4, 4, 5, 5).fives());
-        assertEquals(15, new Yatzy(4, 4, 5, 5, 5).fives());
-        assertEquals(20, new Yatzy(4, 5, 5, 5, 5).fives());
     }
 
     @Test
-    public void sixes_test() {
-        assertEquals(0, new Yatzy(4, 4, 4, 5, 5).sixes());
+    public void should_sum_all_six_dice_for_a_sixes_roll() {
         assertEquals(6, new Yatzy(4, 4, 6, 5, 5).sixes());
-        assertEquals(18, new Yatzy(6, 5, 6, 6, 5).sixes());
     }
 
     @Test
-    public void one_pair() {
+    public void should_sum_the_two_matching_dice_for_a_pair_roll() {
         assertEquals(6, Yatzy.score_pair(3, 4, 3, 5, 6));
-        assertEquals(10, Yatzy.score_pair(5, 3, 3, 3, 5));
-        assertEquals(12, Yatzy.score_pair(5, 3, 6, 6, 5));
-        assertEquals(0, Yatzy.score_pair(1, 3, 4, 6, 5));
-
     }
 
     @Test
-    public void two_Pair() {
+    public void should_score_0_for_a_missed_pair_roll() {
+        assertEquals(0, Yatzy.score_pair(1, 3, 4, 6, 5));
+    }
+
+    @Test
+    public void should_sum_the_four_matching_dice_for_a_two_pair_roll() {
         assertEquals(16, Yatzy.two_pair(3, 3, 5, 4, 5));
-        assertEquals(16, Yatzy.two_pair(3, 3, 5, 5, 5));
+    }
+
+    @Test
+    public void should_score_0_for_a_missed_two_pair_roll() {
         assertEquals(0, Yatzy.two_pair(3, 1, 5, 5, 5));
     }
 
     @Test
-    public void three_of_a_kind() {
+    public void should_sum_the_three_matching_dice_for_a_three_of_a_kind_roll() {
         assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 4, 5));
-        assertEquals(15, Yatzy.three_of_a_kind(5, 3, 5, 4, 5));
-        assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 3, 5));
+    }
+
+    @Test
+    public void should_score_0_for_a_missed_three_of_a_kind_roll() {
         assertEquals(0, Yatzy.three_of_a_kind(1, 3, 4, 6, 5));
-
     }
 
     @Test
-    public void four_of_a_knd() {
+    public void should_sum_the_four_matching_dice_for_a_four_of_a_kind_roll() {
         assertEquals(12, Yatzy.four_of_a_kind(3, 3, 3, 3, 5));
-        assertEquals(20, Yatzy.four_of_a_kind(5, 5, 5, 4, 5));
-        assertEquals(0, Yatzy.four_of_a_kind(1, 3, 4, 6, 5));
-
     }
 
     @Test
-    public void smallStraight() {
-        assertEquals(15, Yatzy.smallStraight(1, 2, 3, 4, 5));
+    public void should_score_0_for_a_missed_four_of_a_kind_roll() {
+        assertEquals(0, Yatzy.four_of_a_kind(1, 3, 4, 6, 5));
+    }
+
+    @Test
+    public void should_score_15_for_a_small_straight_roll() {
         assertEquals(15, Yatzy.smallStraight(2, 3, 4, 5, 1));
+    }
+
+    @Test
+    public void should_score_0_for_a_missed_small_straight_roll() {
         assertEquals(0, Yatzy.smallStraight(1, 2, 2, 4, 5));
     }
 
     @Test
-    public void largeStraight() {
+    public void should_score_20_for_a_large_straight_roll() {
         assertEquals(20, Yatzy.largeStraight(6, 2, 3, 4, 5));
-        assertEquals(20, Yatzy.largeStraight(2, 3, 4, 5, 6));
+    }
+
+    @Test
+    public void should_score_0_for_a_missed_large_straight_roll() {
         assertEquals(0, Yatzy.largeStraight(1, 2, 2, 4, 5));
     }
 
     @Test
-    public void fullHouse() {
+    public void should_sum_all_dice_for_a_full_house_roll() {
         assertEquals(18, Yatzy.fullHouse(6, 2, 2, 2, 6));
+    }
+
+    @Test
+    public void should_score_0_for_a_missed_full_house_roll() {
         assertEquals(0, Yatzy.fullHouse(2, 3, 4, 5, 6));
     }
 }
