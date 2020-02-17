7e427314b9cc44203e090500d44d2899d5c779f2
Robin Litteaut <litteautr@gmail.com>
2020-02-17 18:14:06 +0100

    Adding missing test for complete coverage

--- src/test/java/fr/rli/yatzy/YatzyTest.java
+++ src/test/java/fr/rli/yatzy/YatzyTest.java
@@ -1,6 +1,5 @@
 package fr.rli.yatzy;
 
-import fr.rli.yatzy.Yatzy;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -71,12 +70,15 @@ public class YatzyTest {
         assertEquals(6, Yatzy.score_pair(3, 4, 3, 5, 6));
         assertEquals(10, Yatzy.score_pair(5, 3, 3, 3, 5));
         assertEquals(12, Yatzy.score_pair(5, 3, 6, 6, 5));
+        assertEquals(0, Yatzy.score_pair(1, 3, 4, 6, 5));
+
     }
 
     @Test
     public void two_Pair() {
         assertEquals(16, Yatzy.two_pair(3, 3, 5, 4, 5));
         assertEquals(16, Yatzy.two_pair(3, 3, 5, 5, 5));
+        assertEquals(0, Yatzy.two_pair(3, 1, 5, 5, 5));
     }
 
     @Test
@@ -84,13 +86,16 @@ public class YatzyTest {
         assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 4, 5));
         assertEquals(15, Yatzy.three_of_a_kind(5, 3, 5, 4, 5));
         assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 3, 5));
+        assertEquals(0, Yatzy.three_of_a_kind(1, 3, 4, 6, 5));
+
     }
 
     @Test
     public void four_of_a_knd() {
         assertEquals(12, Yatzy.four_of_a_kind(3, 3, 3, 3, 5));
         assertEquals(20, Yatzy.four_of_a_kind(5, 5, 5, 4, 5));
-        assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 3, 3));
+        assertEquals(0, Yatzy.four_of_a_kind(1, 3, 4, 6, 5));
+
     }
 
     @Test
