70d6292456f1c61f928125d2749ea194e97253b8
Robin Litteaut <litteautr@gmail.com>
2020-02-18 14:54:19 +0100

    Refactoring pair & two_pair

--- src/main/java/fr/rli/yatzy/YatzyCategory.java
+++ src/main/java/fr/rli/yatzy/YatzyCategory.java
@@ -13,15 +13,15 @@ public enum YatzyCategory {
     FOURS(diceRoll -> diceRoll.sumMatchingDiceForDiceValue(4)),
     FIVES(diceRoll -> diceRoll.sumMatchingDiceForDiceValue(5)),
     SIXES(diceRoll -> diceRoll.sumMatchingDiceForDiceValue(6)),
-    PAIR(diceRoll -> calculateScoreForMatchingDice(diceRoll, 2)),
-    TWO_PAIR(YatzyCategory::calculateScoreForTwoPair),
-    THREE_OF_A_KIND(diceRoll -> calculateScoreForMatchingDice(diceRoll, 3)),
-    FOUR_OF_A_KIND(diceRoll -> calculateScoreForMatchingDice(diceRoll, 4)),
+    PAIR(diceRoll -> calculateScoreForMatchingDice(diceRoll, 2, 1)),
+    TWO_PAIR(diceRoll -> calculateScoreForMatchingDice(diceRoll, 2, 2)),
+    THREE_OF_A_KIND(diceRoll -> calculateScoreForMatchingDice(diceRoll, 3, 1)),
+    FOUR_OF_A_KIND(diceRoll -> calculateScoreForMatchingDice(diceRoll, 4, 1)),
     FULL_HOUSE(YatzyCategory::calculateScoreFullHouse),
     SMALL_STRAIGHT(diceRoll -> SMALL_STRAIGHT_VALUE.equals(diceRoll.toOrderedString()) ? SMALL_STRAIGHT_SCORE : MISSED_ROLL_SCORE),
     LARGE_STRAIGHT(diceRoll -> LARGE_STRAIGHT_VALUE.equals(diceRoll.toOrderedString()) ? LARGE_STRAIGHT_SCORE : MISSED_ROLL_SCORE),
     YATZY(diceRoll -> diceRoll.allDiceHaveSameNumber() ? YATZY_SCORE : MISSED_ROLL_SCORE),
-    CHANCE(diceRoll -> diceRoll.sumAllDice());
+    CHANCE(DiceRoll::sumAllDice);
 
     private final ToIntFunction<DiceRoll> scoringRule;
 
@@ -33,35 +33,22 @@ public enum YatzyCategory {
         return scoringRule.applyAsInt(diceRoll);
     }
 
-    private static int calculateScoreForMatchingDice(DiceRoll diceRoll, int nbOfOccurence) {
+    private static int calculateScoreForMatchingDice(DiceRoll diceRoll, int nbOfMatchingDice, int nbOfMatchingFigure) {
         int[] countDiceByValue = diceRoll.countDiceByValue();
 
-        for (int currentDiceValue = MAXIMUM_DICE_VALUE; currentDiceValue > 0; currentDiceValue--) {
-            if (countDiceByValue[currentDiceValue - 1] >= nbOfOccurence) {
-                return currentDiceValue * nbOfOccurence;
-            }
-        }
-
-        return MISSED_ROLL_SCORE;
-    }
-
-    private static int calculateScoreForTwoPair(DiceRoll diceRoll) {
-        int[] countDiceByValue = diceRoll.countDiceByValue();
-
-        int numberOfPairFound = 0;
+        int numberOfFoundFigure = 0;
         int score = 0;
         for (int currentDiceValue = MAXIMUM_DICE_VALUE; currentDiceValue > 0; currentDiceValue--) {
-            if (countDiceByValue[currentDiceValue - 1] >= 2) {
-                numberOfPairFound++;
-                score += currentDiceValue;
+            if (countDiceByValue[currentDiceValue - 1] >= nbOfMatchingDice) {
+                numberOfFoundFigure++;
+                score += currentDiceValue * nbOfMatchingDice;
+                if (numberOfFoundFigure == nbOfMatchingFigure) {
+                    break;
+                }
             }
         }
 
-        if (numberOfPairFound == 2) {
-            return score * numberOfPairFound;
-        } else {
-            return MISSED_ROLL_SCORE;
-        }
+        return numberOfFoundFigure == nbOfMatchingFigure ? score : MISSED_ROLL_SCORE;
     }
 
     public static int calculateScoreFullHouse(DiceRoll diceRoll) {
